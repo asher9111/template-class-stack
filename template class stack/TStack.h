6a199630897ba683183b50026e0b037c3c1b8bdb
Кравченко Владимир <asher1983@yandex.ru>
2021-05-03 14:00:29 +0300

    Add project files.

---
+++ template class stack/TStack.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <iostream>
+
+template <class T>
+class Stack
+{
+public:
+	Stack(int = 10);
+
+	~Stack() { delete[] stackPtr; }
+
+	int push(const T&);
+	int pop(T&);
+	int isEmpty() const { return top == -1; }
+	int isFull() const { return top == size - 1; }
+
+
+private:
+
+	int size;
+	int top;
+	T* stackPtr;
+
+};
